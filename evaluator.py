667f4088ab16f53b9da0124334b2cb5fa9264342
zwanli <zaherwanli@gmail.com>
2017-08-02 09:58:57 +0200

    calculate recall, ndcg

---
+++ evaluator.py
@@ -0,0 +1,363 @@
+#!/usr/bin/env python
+"""
+A module that provides functionalities for calculating error metrics
+and evaluates the given recommender.
+"""
+import numpy
+from top_recommendations import TopRecommendations
+
+
+class Evaluator(object):
+    """
+    A class for computing evaluation metrics and splitting the input data.
+    """
+    def __init__(self, ratings, abstracts_preprocessor=None, random_seed=False,
+                 verbose=False):
+        """
+        Initialize an evaluator array with the initial actual ratings matrix.
+
+        :param int[][] ratings: A numpy array containing the initial ratings.
+        :param AbstractsPreprocessor abstracts_preprocessor: A list of the abstracts.
+        :param bool random_seed: if False, we will use a fixed seed.
+        :param bool verbose: A flag deciding to print progress
+        """
+        self.ratings = ratings
+        self.n_users, self.n_items = ratings.shape
+        if abstracts_preprocessor:
+            self.abstracts_preprocessor = abstracts_preprocessor
+        self.random_seed = random_seed
+        self._verbose = verbose
+        self.k_folds = None
+
+        if self._verbose:
+            print('%d users and %d items' % (self.n_users, self.n_items))
+
+        # stores recommended indices for each user.
+        self.recommendation_indices = [[] for i in range(self.ratings.shape[0])]
+        # False if recommendations have not been loaded yet and vice versa.
+        self.recs_loaded = False
+
+    def get_abstracts_preprocessor(self):
+        """
+        Getter for the Abstracts preprocessor.
+
+        :returns: abstracts preprocessor
+        :rtype: AbstractsPreprocessor
+        """
+        return self.abstracts_preprocessor
+
+    def get_ratings(self):
+        """
+        Getter for the ratings matrix.
+
+        :returns: Ratings matrix
+        :rtype: ndarray
+        """
+        return self.ratings
+
+    def set_kfolds(self, kfolds):
+        """
+        Set the k-folds
+
+        :param int kfolds: the number of the folds in K-fold
+        """
+        self.k_folds = kfolds
+        self.test_percentage = 1.0 / self.k_folds
+
+    def naive_split(self, type='user'):
+        """
+        Split the data into training and testing sets.
+
+        :returns: a tuple of train and test data.
+        :rtype: tuple
+        """
+        if type == 'user':
+            return self.naive_split_users()
+        return self.naive_split_items()
+
+    def naive_split_users(self):
+        """
+        Split the ratings into test and train data for every user.
+
+        :returns: a tuple of train and test data.
+        :rtype: tuple
+        """
+        if self.random_seed is False:
+            numpy.random.seed(42)
+
+        test = numpy.zeros(self.ratings.shape)
+        train = self.ratings.copy()
+        for user in range(self.ratings.shape[0]):
+            non_zeros = self.ratings[user, :].nonzero()[0]
+            test_ratings = numpy.random.choice(non_zeros,
+                                               size=int(self.test_percentage * len(non_zeros)))
+            train[user, test_ratings] = 0.
+            test[user, test_ratings] = self.ratings[user, test_ratings]
+        assert(numpy.all((train * test) == 0))
+        self.test_indices = test
+        return train, test
+
+    def naive_split_items(self):
+        """
+        Split the ratings on test and train data by removing random documents.
+
+        :returns: a tuple of train and test data.
+        :rtype: tuple
+        """
+        if self.random_seed is False:
+            numpy.random.seed(42)
+
+        indices = list(range(self.n_items))
+        test_ratings = numpy.random.choice(indices, size=int(self.test_percentage * len(indices)))
+        train = self.ratings.copy()
+        test = numpy.zeros(self.ratings.shape)
+        for index in test_ratings:
+            train[:, index] = 0
+            test[:, index] = self.ratings[:, index]
+        assert(numpy.all((train * test) == 0))
+        return train, test
+
+    def get_fold(self, fold_num, fold_test_indices):
+        """
+        Returns train and test data for a given fold number
+
+        :param int fold_num: the fold index to be returned
+        :param int[] fold_test_indices: A list of the indicies of the testing fold.
+        :returns: tuple of training and test data
+        :rtype: 2-tuple of 2d numpy arrays
+        """
+        current_test_fold_indices = []
+        index = fold_num
+        for ctr in range(self.ratings.shape[0]):
+            current_test_fold_indices.append(fold_test_indices[index])
+            index += self.k_folds
+        return self.generate_kfold_matrix(current_test_fold_indices)
+
+    def get_kfold_indices(self):
+        """
+        Returns the indices for rating matrix for each kfold split. Where each test set
+        contains ~1/k of the total items a user has in their digital library.
+
+        :returns: a list of all indices of the training set and test set.
+        :rtype: list of lists
+        """
+        if self.random_seed is False:
+            numpy.random.seed(42)
+
+        test_indices = []
+
+        for user in range(self.ratings.shape[0]):
+
+            # Indices for all items in the rating matrix.
+            item_indices = numpy.arange(self.ratings.shape[1])
+
+            # Indices of all items in user's digital library.
+            rated_items_indices = self.ratings[user].nonzero()[0]
+            mask = numpy.ones(len(self.ratings[user]), dtype=bool)
+            mask[[rated_items_indices]] = False
+            # Indices of all items not in user's digital library.
+            non_rated_indices = item_indices[mask]
+
+            # Shuffle all rated items indices
+            numpy.random.shuffle(rated_items_indices)
+
+            # Size of 1/k of the total user's ratings
+            size_of_test = round((1.0 / self.k_folds) * len(rated_items_indices))
+
+            # 2d List that stores all the indices of each test set for each fold.
+            test_ratings = [[] for x in range(self.k_folds)]
+
+            counter = 0
+            numpy.random.shuffle(non_rated_indices)
+            # List that stores the number of indices to be added to each test set.
+            num_to_add = []
+
+            # create k different folds for each user.
+            for index in range(self.k_folds):
+                if index == self.k_folds - 1:
+                    test_ratings[index] = numpy.array(rated_items_indices[counter:len(rated_items_indices)])
+                else:
+                    test_ratings[index] = numpy.array(rated_items_indices[counter:counter + size_of_test])
+                counter += size_of_test
+
+                # adding unique zero ratings to each test set
+                num_to_add.append(int((self.ratings.shape[1] / self.k_folds) - len(test_ratings[index])))
+                if index > 0 and num_to_add[index] != num_to_add[index - 1]:
+                    addition = non_rated_indices[index * (num_to_add[index - 1]):
+                                                         (num_to_add[index - 1] * index) + num_to_add[index]]
+                else:
+                    addition = non_rated_indices[index * (num_to_add[index]):num_to_add[index] * (index + 1)]
+
+                test_ratings[index] = numpy.append(test_ratings[index], addition)
+                test_indices.append(test_ratings[index])
+
+        self.test_indices = test_indices
+        return test_indices
+
+    def generate_kfold_matrix(self, test_indices):
+        """
+        Returns a training set and a training set matrix for one fold.
+        This method is to be used in conjunction with get_kfold_indices()
+
+        :param int[] test_indices: array of test set indices.
+        :returns: Training set matrix and Test set matrix.
+        :rtype: 2-tuple of 2d numpy arrays
+        """
+        train_matrix = numpy.zeros(self.ratings.shape)
+        test_matrix = numpy.zeros(self.ratings.shape)
+        for user in range(train_matrix.shape[0]):
+            train_indices = list(set(range(self.n_items)) - set(test_indices[user]))
+            test_matrix[user, test_indices[user]] = self.ratings[user, test_indices[user]]
+            train_matrix[user, train_indices] = self.ratings[user, train_indices]
+
+        return train_matrix, test_matrix
+
+    def load_top_recommendations(self, n_recommendations, predictions, test_data, fold):
+        """
+        This method loads the top n recommendations into a local variable.
+
+        :param int n_recommendations: number of recommendations to be generated.
+        :param int[][] predictions: predictions matrix (only 0s or 1s)
+        :returns: A matrix of top recommendations for each user.
+        :rtype: int[][]
+        """
+        for user in range(self.ratings.shape[0]):
+            nonzeros = self.test_indices[(user * (1 + fold))]
+            top_recommendations = TopRecommendations(n_recommendations)
+            for index in nonzeros:
+                index = int(index)
+                top_recommendations.insert(index, predictions[user][index])
+            self.recommendation_indices[user] = list(reversed(top_recommendations.get_indices()))
+            top_recommendations = None
+
+        self.recs_loaded = True
+        return self.recommendation_indices
+
+    def load_top_recommendations_2(self, n_recommendations, predictions, test_data):
+        """
+        This method loads the top n recommendations into a local variable.
+
+        :param int n_recommendations: number of recommendations to be generated.
+        :param int[][] predictions: predictions matrix (only 0s or 1s)
+        :returns: A matrix of top recommendations for each user.
+        :rtype: int[][]
+        """
+        self.test_indices = test_data
+        for user in range(self.ratings.shape[0]):
+            nonzeros = self.test_indices[user].nonzero()
+            top_recommendations = TopRecommendations(n_recommendations)
+            for index in nonzeros[0]:
+                index = int(index)
+                top_recommendations.insert(index, predictions[user][index])
+            self.recommendation_indices[user] = list(reversed(top_recommendations.get_indices()))
+            top_recommendations = None
+
+        self.recs_loaded = True
+        return self.recommendation_indices
+
+    def get_rmse(self, predicted, actual=None):
+        """
+        The method given a prediction matrix returns the root mean squared error (rmse).
+
+        :param float[][] predicted: numpy matrix of floats representing the predicted ratings
+        :returns: root mean square error
+        :rtype: float
+        """
+        if actual is None:
+            actual = self.ratings
+
+        rss = 0
+        for i in range(predicted.shape[0]):
+            rss += numpy.sum((predicted[i] - actual[i]) ** 2)
+        rss = float(rss) / numpy.size(predicted)
+
+        return numpy.sqrt(rss)
+
+    def calculate_recall(self, ratings, predictions):
+        """
+        The method given original ratings and predictions returns the recall of the recommender
+
+        :param int[][] ratings: ratings matrix
+        :param int[][] predictions: predictions matrix (only 0s or 1s)
+        :returns: recall, ranges from 0 to 1
+        :rtype: float
+        """
+        denom = sum(sum(ratings))
+        nonzeros = ratings.nonzero()
+        nonzeros_predictions = predictions[nonzeros]
+        return sum(nonzeros_predictions) / denom  # Division by zeros are handled.
+
+    def recall_at_x(self, x, predictions, ratings, rounded_predictions):
+        """
+        The method calculates the average recall of all users by only looking at the top x
+        and the normalized Discounted Cumulative Gain.
+
+        :param int x: number of recommendations to look at, sorted by relevance.
+        :param int[][] ratings: ratings matrix
+        :param float[][] predictions: calculated predictions of the recommender.
+        :param int[][] test_data: test data.
+        :returns: Recall at x
+        :rtype: float
+        """
+
+        recalls = []
+        for user in range(ratings.shape[0]):
+            recommendation_hits = 0
+            user_likes = ratings[user].sum()
+            recall = 0
+            if user_likes != 0:
+                recommendation_hits = (self.ratings[user][self.recommendation_indices[user][:x]] *
+                                       rounded_predictions[user][self.recommendation_indices[user][:x]]).sum()
+
+                recall = recommendation_hits / (min(x, user_likes) * 1.0)
+                recalls.append(recall)
+        return numpy.mean(recalls, dtype=numpy.float16)
+
+    def calculate_ndcg(self, n_recommendations, rounded_predictions):
+        """
+        The method calculates the normalized Discounted Cumulative Gain of all users
+        by only looking at the top n_recommendations.
+
+        :param int n_recommendations: number of recommendations to look at, sorted by relevance.
+        :param float[][] predictions: calculated predictions of the recommender
+        :returns: nDCG for n_recommendations
+        :rtype: float
+        """
+        ndcgs = []
+        for user in range(self.ratings.shape[0]):
+            dcg = 0
+            idcg = 0
+            for pos_index, index in enumerate(self.recommendation_indices[user]):
+                dcg += (self.ratings[user, index] * rounded_predictions[user][index]) / numpy.log2(pos_index + 2)
+                idcg += 1 / numpy.log2(pos_index + 2)
+                if pos_index + 1 == n_recommendations:
+                    break
+            if idcg != 0:
+                ndcgs.append(dcg / idcg)
+        return numpy.mean(ndcgs, dtype=numpy.float16)
+
+    def calculate_mrr(self, n_recommendations, predictions, test_data, rounded_predictions):
+        """
+        The method calculates the mean reciprocal rank for all users
+        by only looking at the top n_recommendations.
+
+        :param int n_recommendations: number of recommendations to look at, sorted by relevance.
+        :param float[][] predictions: calculated predictions of the recommender
+        :returns: mrr at n_recommendations
+        :rtype: float
+        """
+
+        mrr_list = []
+
+        for user in range(self.ratings.shape[0]):
+            mrr = 0
+            for mrr_index, index in enumerate(self.recommendation_indices[user]):
+                score = self.ratings[user][index] * rounded_predictions[user][index]
+                if score == 1:
+                    mrr = score / (mrr_index + 1)
+                    break
+                if mrr_index + 1 == n_recommendations:
+                    break
+            mrr_list.append(mrr)
+
+        return numpy.mean(mrr_list, dtype=numpy.float16)
--- train.py
+++ train.py
@@ -1,11 +1,10 @@
 import tensorflow as tf
-
 import argparse
 import time
 import os
 import numpy as np
 from six.moves import cPickle
-
+from evaluator import Evaluator
 from utils import textloader
 from model import Model
 import pickle
@@ -22,9 +21,11 @@ def main():
                        help='directory containing tensorboard logs')
     parser.add_argument('--save_dir', type=str, default='save',
                        help='directory to store checkpointed models')
-    parser.add_argument('--rnn_size', type=int, default=256,
+    parser.add_argument('--rnn_size', type=int, default=200,
                        help='size of RNN hidden state')
-    parser.add_argument('--num_layers', type=int, default=3,
+    parser.add_argument('--embedding_dim', type=int, default=200,
+                        help='dimension of the embeddings')
+    parser.add_argument('--num_layers', type=int, default=2,
                        help='number of layers in the RNN')
     parser.add_argument('--model', type=str, default='gru',
                        help='rnn, gru, or lstm')
@@ -32,7 +33,7 @@ def main():
                        help='minibatch size')
     parser.add_argument('--seq_length', type=int, default=20,
                        help='RNN sequence length')
-    parser.add_argument('--num_epochs', type=int, default=5000,
+    parser.add_argument('--num_epochs', type=int, default=200,
                        help='number of epochs')
     parser.add_argument('--save_every', type=int, default=1000,
                        help='save frequency')
@@ -61,14 +62,15 @@ def train(args):
     data_loader = textloader(args.embedding_dir,args.batch_size)
     args.vocab_size = data_loader.vocab_size
 
-    if os.path.exists('/home/wanli/repositories/atrnn_prototype/abstracrs_word_embeddings.pkl'):
+
+    if os.path.exists('abstracrs_word_embeddings_dummy.pkl'):
         print('Loading abstracts')
-        with open('/home/wanli/repositories/atrnn_prototype/abstracrs_word_embeddings.pkl', 'rb') as f:
+        with open('abstracrs_word_embeddings_dummy.pkl', 'rb') as f:
             data_loader.all_documents= pickle.load(f)
     else:
-        text_dir = '/home/wanli/data/Extended_ctr/dummy/raw-data.csv'
-        data_loader.read_abstrcts(text_dir)
-        with open("abstracrs_word_embeddings.pkl",'wb') as f:
+        text_dir = '/home/wanli/data/Extended_ctr/citeulike_t_extended/raw-data.csv'
+        data_loader.read_abstracts(text_dir)
+        with open("abstracts_word_embeddings_dummy_citeulike_t_extended.pkl",'wb') as f:
             pickle.dump(data_loader.all_documents,f,pickle.HIGHEST_PROTOCOL)
             print("Saved abstracts")
     #Read ratings input
@@ -78,10 +80,13 @@ def train(args):
     # for u,v,r,docs in data_loader.generate_batches(10000):
     #     print(len(docs))
     args.seq_length = data_loader.get_min_max_length()
-    model = Model(args, data_loader.M)
+    model = Model(args, data_loader.M,data_loader.embed)
 
-    def construct_feed(u_idx, v_idx, r, docs, batch_size):
-        return {model.u_idx: u_idx, model.v_idx: v_idx, model.r: r, model.input_data: docs, model.batch_size: batch_size}
+    def construct_feed(u_idx, v_idx, r, docs,bi_hid_fw, bi_hid_bw, batch_size):
+        return {model.u_idx: u_idx, model.v_idx: v_idx, model.r: r, model.input_data: docs,
+                model.init_state_fw: bi_hid_fw, model.init_state_bw: bi_hid_bw,model.batch_size: batch_size}
+                # model.initial_state: hid_state,
+
         # check compatibility if training is continued from previously saved model
     if args.init_from is not None:
         # check if all necessary files exist
@@ -111,50 +116,77 @@ def train(args):
     #     cPickle.dump((data_loader.word_to_id, data_loader.vocab), f)
 
 
-    merged = tf.summary.merge_all()
-    train_writer = tf.summary.FileWriter(args.log_dir+ '/train')
-    valid_writer = tf.summary.FileWriter(args.log_dir + '/validation')
-    test_writer = tf.summary.FileWriter(args.log_dir + '/test')
+    dir_prefix = time.strftime("%d:%m-%H:%M:")
+    train_writer = tf.summary.FileWriter(args.log_dir+ '/{0}-train'.format(dir_prefix))
+    valid_writer = tf.summary.FileWriter(args.log_dir + '/{0}-validation'.format(time.strftime(dir_prefix)))
+    test_writer = tf.summary.FileWriter(args.log_dir + '/{0}-test'.format(time.strftime(dir_prefix)))
     best_val_rmse = np.inf
     best_val_mae = np.inf
     best_test_rmse = 0
     best_test_mae = 0
 
-    gpu_options = tf.GPUOptions(per_process_gpu_memory_fraction=args.gpu_mem)
+    evaluator = Evaluator(data_loader.M,verbose=True)
 
+    gpu_options = tf.GPUOptions(per_process_gpu_memory_fraction=args.gpu_mem)
     n_steps = args.num_epochs
     with tf.Session(config=tf.ConfigProto(gpu_options=gpu_options)) as sess:
-        train_writer.add_graph(sess.graph)
-        valid_writer.add_graph(sess.graph)
-        test_writer.add_graph(sess.graph)
+        # train_writer.add_graph(sess.graph)
+        # valid_writer.add_graph(sess.graph)
+        # test_writer.add_graph(sess.graph)
         tf.global_variables_initializer().run()
-        saver = tf.train.Saver(tf.global_variables())
+        model.saver = tf.train.Saver(tf.global_variables())
+
+        bi_state_fw = sess.run(model.init_state_bw,feed_dict={model.batch_size: args.batch_size})
+        bi_state_bw = sess.run(model.init_state_fw,feed_dict={model.batch_size: args.batch_size})
+        h_state = sess.run(model.initial_state,feed_dict={model.batch_size: args.batch_size})
+
         for u, v, r, d, step in data_loader.generate_batches(n_steps):
-            # feed = {model.u_idx: u, model.v_idx: v, model.r: r, model.input_data: d}
-            feed = construct_feed(u,v,r,d,args.batch_size)
-            # model.Yr.eval(feed_dict={model.input_data: d})
+
+            feed = construct_feed(u, v, r, d, bi_state_fw, bi_state_bw,args.batch_size)
             sess.run(model.train_step_v, feed_dict=feed)
             sess.run(model.train_step_u, feed_dict=feed)
-            _, rmse, mae, summary_str = sess.run([model.train_step_rnn, model.RMSE,model.MAE, model.summary_op], feed_dict=feed)
+            _, U,V ,U_b ,V_b , bi_out_fw, bi_out_bw, final_state, rmse, mae, summary_str = sess.run([model.train_step_rnn,
+                                                                                           model.U,model.V,model.U_bias,model.V_bias,
+                                                                                           model.bi_output_state_fw, model.bi_output_state_bw, model.H,
+                                                                                           model.RMSE,model.MAE, model.summary_op],
+                                                        feed_dict=feed)
             train_writer.add_summary(summary_str,step)
-
 
             if step % int(n_steps / 100) == 0:
                 valid_u_idx, valid_v_idx, valid_m, valid_docs = data_loader.get_valid_idx()
-                # feed_dict = {model.u_idx: valid_u_idx,model.v_idx: valid_v_idx, model.r:valid_m, model.input_data: valid_docs}
-                feed_dict = construct_feed(valid_u_idx, valid_v_idx, valid_m, valid_docs, valid_docs.shape[0])
+                valid_bi_fw = sess.run(model.init_state_fw,feed_dict={model.batch_size:valid_docs.shape[0] })
+                valid_bi_bw = sess.run(model.init_state_bw,feed_dict={model.batch_size:valid_docs.shape[0] })
+                init_state = sess.run(model.initial_state,feed_dict={model.batch_size:valid_docs.shape[0] })
+                feed_dict = construct_feed(valid_u_idx, valid_v_idx, valid_m, valid_docs,valid_bi_fw, valid_bi_bw, valid_docs.shape[0])
                 rmse_valid, mae_valid, summary_str = sess.run(
                     [model.RMSE, model.MAE, model.summary_op], feed_dict=feed_dict)
-
                 valid_writer.add_summary(summary_str, step)
 
                 test_u_idx, test_v_idx, test_m, test_docs = data_loader.get_test_idx()
-                # feed_dict = {model.u_idx:test_u_idx,model.v_idx:  test_v_idx, model.r:test_m, model.input_data: test_docs}
-                feed_dict= construct_feed(test_u_idx, test_v_idx, test_m, test_docs, test_docs.shape[0])
+                test_bi_fw = sess.run(model.init_state_fw, feed_dict={model.batch_size: test_docs.shape[0]})
+                test_bi_bw = sess.run(model.init_state_bw, feed_dict={model.batch_size: test_docs.shape[0]})
+                init_state = sess.run(model.initial_state,feed_dict={model.batch_size:test_docs.shape[0] })
+                feed_dict= construct_feed(test_u_idx, test_v_idx, test_m, test_docs,test_bi_fw,test_bi_bw, test_docs.shape[0])
                 rmse_test, mae_test, summary_str = sess.run(
                     [model.RMSE, model.MAE, model.summary_op], feed_dict=feed_dict)
-
                 test_writer.add_summary(summary_str, step)
+
+                prediction_matrix = np.matmul(U,V.T)
+                prediction_matrix = np.add(prediction_matrix,np.reshape(U_b,[-1,1]))
+                prediction_matrix = np.add(prediction_matrix,V_b)
+                rounded_predictions = data_loader.rounded_predictions(prediction_matrix)
+                testM = np.zeros(data_loader.M.shape)
+                testM[data_loader.nonzero_u_idx[data_loader.test_idx], data_loader.nonzero_v_idx[data_loader.test_idx]] = data_loader.M[
+                    data_loader.nonzero_u_idx[data_loader.test_idx], data_loader.nonzero_v_idx[data_loader.test_idx]]
+
+                evaluator.load_top_recommendations_2(200,prediction_matrix,testM)
+                recall_10 = evaluator.recall_at_x(10, prediction_matrix, data_loader.M,rounded_predictions )
+                recall_50 = evaluator.recall_at_x(50, prediction_matrix, data_loader.M, rounded_predictions)
+                recall_100 = evaluator.recall_at_x(100, prediction_matrix, data_loader.M, rounded_predictions)
+                recall_200 = evaluator.recall_at_x(200, prediction_matrix, data_loader.M, rounded_predictions)
+                recall = evaluator.calculate_recall(ratings=data_loader.M,predictions=rounded_predictions)
+                ndcg_at_five = evaluator.calculate_ndcg(5, rounded_predictions)
+                ndcg_at_ten = evaluator.calculate_ndcg(10, rounded_predictions)
 
                 print("Step {0} | Train RMSE: {1:3.4f}, MAE: {2:3.4f}".format(
                     step, rmse, mae))
@@ -162,6 +194,16 @@ def train(args):
                     rmse_valid, mae_valid))
                 print("         | Test  RMSE: {0:3.4f}, MAE: {1:3.4f}".format(
                     rmse_test, mae_test))
+                print("         | Recall@10: {0:3.4f}".format(recall_10))
+                print("         | Recall@50: {0:3.4f}".format(recall_50))
+                print("         | Recall@100: {0:3.4f}".format(recall_100))
+                print("         | Recall@200: {0:3.4f}".format(recall_200))
+                print("         | Recall: {0:3.4f}".format(recall))
+                print("         | ndcg@5: {0:3.4f}".format(ndcg_at_five))
+                print("         | ndcg@10: {0:3.4f}".format(ndcg_at_ten))
+
+
+
 
                 if best_val_rmse > rmse_valid:
                     best_val_rmse = rmse_valid
@@ -171,6 +213,15 @@ def train(args):
                     best_val_mae = mae_valid
                     best_test_mae = mae_test
 
+            # loop state around
+            h_state = final_state
+            bi_state_fw = bi_out_fw
+            bi_state_bw = bi_out_bw
+            # if step > 0 and (step % args.save_every == 0 or ( step == args.num_epochs - 1)):  # save for the last result
+            #     checkpoint_path = os.path.join(args.save_dir, 'model.ckpt')
+            #     saver.save(sess, checkpoint_path, global_step=step)
+            #     print("model saved to {}".format(checkpoint_path))
+        model.saver.save(sess, args.log_dir+ "/{0}model.ckpt".format(time.strftime(dir_prefix)))
         print('Best test rmse:',best_test_rmse,'Best test mae',best_test_mae,sep=' ')
         # restore model
         # if args.init_from is not None:
@@ -200,6 +251,8 @@ def train(args):
         #             saver.save(sess, checkpoint_path, global_step = e * data_loader.num_batches + b)
         #             print("model saved to {}".format(checkpoint_path))
         train_writer.close()
+        valid_writer.close()
+        test_writer.close()
 
 if __name__ == '__main__':
     main()
