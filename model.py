fix prediction matrix calculation, add FC to join the rnn and att modules outputs

model.py
@@ -38,7 +38,10 @@ class Model():
         self.batch_pointer = tf.Variable(0, name="batch_pointer", trainable=False, dtype=tf.int32)
         self.inc_batch_pointer_op = tf.assign(self.batch_pointer, self.batch_pointer + 1)
 
-        outputs,init_ops = get_input_dataset(train_filename,test_filename, batch_size=self.batch_size)
+        self.train_filename = tf.placeholder_with_default(train_filename,shape=(),name='train_filename')
+        self.test_filename = tf.placeholder_with_default(test_filename,shape=(),name='test_filename')
+
+        outputs,init_ops = get_input_dataset(self.train_filename,self.test_filename, batch_size=self.batch_size)
         self.u_idx,self.v_idx, self.r, self.input_text, self.seq_lengths = outputs
 
         confidence = tf.constant(confidence_matrix, dtype=tf.float32, shape=confidence_matrix.shape,
@@ -135,19 +138,16 @@ class Model():
 
             # RNN output layer:
             # avg pool layer [batch_size, embedding_dim]
-            self.G = tf.reduce_mean(self.Yr, 1)
+            self.rnn_output = tf.reduce_mean(self.Yr, 1)
 
-        #Update RNN output
-        with tf.device("/cpu:0"):
-            # RNN output [num_items, embedding_dim]
-            self.RNN = tf.get_variable(shape=[self.m, self.k], name='RNN_output', trainable=False, dtype=tf.float32
-                                       ,initializer=tf.constant_initializer(0.))
-            self.update_rnn_output = tf.scatter_update(self.RNN, self.v_idx, self.G)
 
         use_attribues= True
+        sum_joint_output = False
+        fc_joint_output = True
+        att_ouput_dim = 50
         if use_attribues:
             ## Attributes component
-            self.att_output = self.attribute_module(features_matrix,args.num_layers)
+            self.att_output = self.attribute_module(features_matrix,args.num_layers,(att_ouput_dim if fc_joint_output else self.k))
 
         # Free some ram
         del features_matrix
@@ -174,10 +174,21 @@ class Model():
 
 
         with tf.name_scope('joint_output'):
-            self.F = tf.add(self.V_embed,self.G)
-
             if use_attribues:
-                self.F = tf.add(self.F,self.att_output)
+                if fc_joint_output:
+                    # concatonate the rnn output and the attributes output
+                    self.Q = tf.concat([self.rnn_output, self.att_output], 1)
+                    self.fc_layer = tf.layers.dense(inputs=self.Q, units=self.k, activation=tf.nn.relu)
+                    self.F = tf.add(self.V_embed, self.fc_layer)
+
+                elif sum_joint_output:
+                    self.F = tf.add(self.V_embed, self.rnn_output)
+                    self.F = tf.add(self.F, self.att_output)
+            else:
+                self.F = tf.add(self.V_embed, self.rnn_output)
+
+
+
 
             self.r_hat = tf.reduce_sum(tf.multiply(self.U_embed, self.F), reduction_indices=1)
 
@@ -187,18 +198,47 @@ class Model():
 
             multi_task = args.multi_task
 
+        with tf.name_scope('Tag_prediction'):
             if multi_task:
                 # Tag prediction task
                 tags_loss = self.tag_module(tags_count,self.k)
 
-        # Update predicted ratings matrix
-        with tf.device("/cpu:0"):
-            # RNN output [num_items, embedding_dim]
-            self.predicted_matrix = tf.get_variable(shape=[self.n, self.m], name='Predicted_ratings',
-                                                    trainable=False, dtype=tf.float32
-                                                    , initializer=tf.constant_initializer(0.))
-            self.update_predicted_matrix = tf.scatter_nd_update(self.predicted_matrix,
-                                                                        indices=u_v_idx, updates=self.r_hat)
+
+        with tf.name_scope('update_doc_att_embedding'):            # RNN output [num_items, embedding_dim]
+            # Update RNN and/or attribute joint output
+            with tf.device("/cpu:0"):
+                # RNN and/or attribute joint output [num_items, embedding_dim]
+                self.doc_embed = tf.get_variable(shape=[self.m, self.k], name='doc_embed', trainable=False,
+                                                 dtype=tf.float32
+                                                 , initializer=tf.constant_initializer(0.))
+                self.update_doc_embed = tf.scatter_update(self.doc_embed, self.v_idx, self.rnn_output)
+
+                if use_attribues:
+                    self.att_embed = tf.get_variable(shape=[self.m, (att_ouput_dim if fc_joint_output else self.k)],
+                                                     name='att_embed', trainable=False, dtype=tf.float32
+                                                 , initializer=tf.constant_initializer(0.))
+                    self.update_att_embed = tf.scatter_update(self.att_embed, self.v_idx, self.att_output)
+
+
+                    self.joint_doc_att_embed = tf.get_variable(shape=[self.m,  self.k],
+                                                     name='joint_doc_att_embed', trainable=False, dtype=tf.float32
+                                                 , initializer=tf.constant_initializer(0.))
+                    self.update_doc_att_embed = tf.scatter_update(self.joint_doc_att_embed, self.v_idx, self.fc_layer)
+
+        with tf.name_scope('Calculate_prediction_matrix'):
+            # Get predicted ratings matrix
+            if use_attribues:
+                self.R_hat = tf.add(self.V, self.joint_doc_att_embed)
+            else:
+                self.R_hat = tf.add(self.V, self.doc_embed)
+
+            self.R_hat = tf.matmul(self.U, self.R_hat,transpose_b=True)
+
+            # self.r_hat = tf.reduce_sum(tf.multiply(self.U_embed, self.V_embed), reduction_indices=1)
+            self.R_hat = tf.add(self.R_hat, tf.reshape(self.U_bias,shape=[-1,1]))
+            self.get_prediction_matrix = tf.add(self.R_hat, self.V_bias, name="Prediction_matrix")
+
+
         with tf.name_scope('loss'):
             # Loss function
             # self.MAE = tf.reduce_mean(tf.abs(tf.subtract(self.r, self.r_hat)))
@@ -268,13 +308,14 @@ class Model():
         self.saver = tf.train.Saver()
 
 
-    def attribute_module(self, features_matrix,  n_layers, ):
+    def attribute_module(self, features_matrix,  n_layers, output_dim):
         '''
 
         :param input:
         :param n_layers:
         :return:
         '''
+
         # Implementation of a simple MLP network with one hidden layer.
         x_size = features_matrix.shape[1]
 
@@ -296,7 +337,7 @@ class Model():
         n_hidden_1 = int(self.training_samples_count / (alpha * (x_size + self.k)))  # 1st layer number of neurons
         n_hidden_2 = int(self.training_samples_count / (alpha * (x_size + self.k)))
         n_hidden_3 = int(self.training_samples_count / (alpha * (x_size + self.k)))  # 1st layer number of neurons
-        y_size = self.k
+        y_size = output_dim
 
 
         with tf.variable_scope('Attributes_component_%d-layers' % (n_layers)):
@@ -351,7 +392,7 @@ class Model():
 
             tags_probalities = tf.einsum('ai,bi->ab',self.F, tags_embeddings)
 
-            # todo: add downweights for predicting the unobserved tags
+            # todo: add down weights for predicting the unobserved tags
 
             tags_loss = tf.losses.sigmoid_cross_entropy(tags_actual,tags_probalities,)
         return tags_loss

predict.py
@@ -71,16 +71,17 @@ def predict(args):
     if args.init_from is not None:
         # check if all necessary files exist
         assert os.path.isdir(args.init_from), " %s must be a directory" % args.init_from
-        assert os.path.isfile(
-            os.path.join(args.init_from, "config.pkl")), "config.pkl file does not exist in path %s" % args.init_from
+        # assert os.path.isfile(
+        #     os.path.join(args.init_from, "config.pkl")), "config.pkl file does not exist in path %s" % args.init_from
 
         ckpt = tf.train.get_checkpoint_state(args.init_from)
         assert ckpt, "No checkpoint found"
         assert ckpt.model_checkpoint_path, "No model path found in checkpoint"
 
         # open old config and check if models are compatible
-        with open(os.path.join(args.init_from, 'config.pkl'), 'rb') as f:
-            saved_model_args = pickle.load(f)
+        # with open(os.path.join(args.init_from, 'config.pkl'), 'rb') as f:
+        #     saved_model_args = pickle.load(f)
+
         # need_be_same = ["model", "rnn_size", "num_layers", "seq_length"]
         # for checkme in need_be_same:
         #     assert vars(saved_model_args)[checkme] == vars(args)[
@@ -94,5 +95,15 @@ def predict(args):
         # assert saved_words == data_loader.words, "Data and loaded model disagree on word set!"
         # assert saved_vocab == data_loader.vocab, "Data and loaded model disagree on dictionary mappings!"
 
+        with tf.Session() as sess:
+            meta_graph = os.path.join(args.init_from, '04:11-08:54:-citeulike-a-warm-300-f5model.ckpt.meta')
+            saver = tf.train.import_meta_graph(meta_graph)
+            saver.restore(sess, tf.train.latest_checkpoint(args.init_from))
+            rnn = sess.run(['RNN_output:0'])
+            print ([n.name for n in tf.get_default_graph().as_graph_def().node])
 
     # model = Model(args, rating_matrix, embeddings, confidence, train_filename=,test_filename=)
+
+
+if __name__ == '__main__':
+    main()

test_cases.py
@@ -987,6 +987,8 @@ def test_loss():
     print(RMSE.eval())
     print(l2_loss.eval())
 
+def fn():
+    return 1,2,3,4
 
 def main():
     # batch_size = 1
@@ -1103,12 +1105,22 @@ def main():
 
     # test_tf_scatter_update()
 
-    # score_file ='/home/wanli/data/Extended_ctr/citeulike_a_extended/outof-matrix-item_folds/fold-5/score.npy'
+    # score_file ='/home/wanli/data/Extended_ctr/citeulike_a_extended/in-matrix-item_folds/fold-1/score.npy'
     # score = np.load(score_file)
     # a = 1
 
-    test_loss()
+    # # b = np.array(np.random.rand(2,3),dtype=np.float64)
+    # b = [[ 0.76327468e-10,  0.36366175,  0.3757063 ],[ 0.5597608,   0.77586819,  0.35938623]]
+    # np.save('b',b)
+    # print(b)
+    #
+    # b= np.load('b.npy')
+    #
+    # print(b)
 
+    # test_loss()
+
+    b = fn()
 
 if __name__ == '__main__':
     main()

train.py
@@ -54,6 +54,9 @@ def main():
                         help='save frequency')
     parser.add_argument('--multi_task', action='store_true',
                         help='Multi-task learning')
+    parser.add_argument('--use_att', action='store_true',
+                        help='Learn attribute embeddins')
+
     parser.add_argument('--learning_rate', type=float, default=0.000001,
                         help='learning rate')
 
@@ -324,31 +327,41 @@ def train(args):
             # for step in range(1):
                 print('{0}: Epoch {1}'.format(time.strftime("%d:%m-%H:%M:"), step))
                 print('Training .....................................')
+
                 # Initialize the training dataset iterator
                 sess.run(model.train_init_op)
+
                 feed_dict = construct_feed(bi_state_fw, bi_state_bw,
                                            dropout_embed_layer, dropout_bidir_layer, dropout_second_layer)
 
                 fetches = [model.joint_train_step,
-                           model.update_rnn_output,
-                           model.u_idx, model.v_idx,
-                           model.U, model.V, model.RNN, model.U_bias, model.V_bias, model.att_output,
-                           model.update_predicted_matrix,
+                           # model.u_idx, model.v_idx,
+                           # model.U, model.V, model.doc_embed, model.U_bias, model.V_bias, model.att_output,
                            model.bi_output_state_fw, model.bi_output_state_bw, model.H,
-                           model.RMSE, model.summary_op, model.inc_batch_pointer_op]
+                           model.RMSE, model.summary_op, model.inc_batch_pointer_op,
+                           model.update_doc_embed]
+                if args.use_att:
+                    fetches.extend([model.update_att_embed, model.update_doc_att_embed])
+
                 start = time.time()
 
                 for batch in range(nb_batches_train):
                 # for batch in range(1):
-                    _, _, u_idx, v_idx, U, V, rnn, U_b, V_b, att_ouput, \
-                    prediction_matrix, bi_out_fw, bi_out_bw, final_state, rmse, summary_str, _ = \
+                    # u_idx, v_idx, U, V, rnn, U_b, V_b, att_ouput, \
+                    fetched = \
                         sess.run(fetches, feed_dict=feed_dict)
+                    if args.use_att:
+                        _, bi_out_fw, bi_out_bw, final_state, rmse, summary_str, _, doc_embed, att_embed, joint_embed = fetched
+
+                    else:
+                        _,bi_out_fw, bi_out_bw, final_state, rmse, summary_str, _, doc_embed = fetched
                     # print every 500 iteration
                     if batch // 10 % 50 == 0:
                         print("Epoch {0}, batch {1}".format(step, batch))
                     train_writer.add_summary(summary_str, global_step=(step * nb_batches_train + batch))
                 end = time.time()
                 print('Epoch {0}, finished in {1}'.format(step, end - start))
+
                 # todo: check the condition
                 if step // 1 % 5 == 0:
                     print('{0}:Validation ............'.format(time.strftime("%d:%m-%H:%M:")))
@@ -364,9 +377,8 @@ def train(args):
                         init_state = sess.run(model.initial_state)
                         # don't dropout
                         feed_dict = construct_feed(test_bi_fw, test_bi_bw, 0, 0, 0)
-                        for batch in range(2):
-                            # for batch in range(nb_batches_val):
-
+                        # for batch in range(2):
+                        for batch in range(nb_batches_val):
                             rmse_test, summary_str = sess.run(
                                 [model.RMSE, model.summary_op], feed_dict=feed_dict)
                             test_writer.add_summary(summary_str, global_step=(step * nb_batches_val + batch))
@@ -414,6 +426,7 @@ def train(args):
                 bi_state_fw = bi_out_fw
                 bi_state_bw = bi_out_bw
 
+            prediction_matrix = sess.run(model.get_prediction_matrix)
             fold_dir = os.path.dirname(os.path.dirname(path_training))
             predicted_ratings_file = os.path.join(fold_dir, 'score.npy')
             np.save(predicted_ratings_file, prediction_matrix)